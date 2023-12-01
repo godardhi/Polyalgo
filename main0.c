9394d5cdf8e030b3cce1fa0e6787957461d42fed
Godard H.I <42699475+godardhi@users.noreply.github.com>
2023-12-01 12:11:09 +0100

    Master mind game version 2

---
+++ main0.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+int masterMind(char jouer1[][20], char jouer2[][20], int Nmb_couleurs);
+void code_couleur(char jouer2[][20], int Nmb_couleurs);
+void afficher(int bool);
+
+
+int main()
+{
+	// Début du jeux
+	printf("\t------------------------------------------\n");
+	printf("\t---------Bienvenu sur Master Mind---------\n");
+	printf("\t------------------------------------------\n");
+
+	printf("\t\t**Consignes du jeux**:\n");
+	printf("\n\n");
+	printf("1.les entres doivent être en miniscule\n");
+
+	printf("2.Les couleurs possible (rouge, bleu, vert, jaune, violet, orange, rose, marron, gris, noir, blanc)\n");
+
+	char oui_non;
+	printf("\n");
+	printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+	printf("Taper o pour commencer le jeux ou q pour quitter:\n");
+	while(oui_non!='o' && oui_non!='q')
+	{
+		printf("~");
+		scanf(" %c", &oui_non);
+	}
+
+	if(oui_non=='o')
+	{
+		// declaration des variables
+
+		int Nmb_couleurs;
+
+		// Le jouer 2 doit choisir combien le Nmb_couleurs
+
+		printf("Combien des couleurs voulez vous dévine: ");
+		scanf(" %d",&Nmb_couleurs);
+		printf("\n\n");
+		// declaration de tableau de longeur=Nmb_couleurs
+	
+		char jouer1[Nmb_couleurs][20];
+		char jouer2[Nmb_couleurs][20];
+
+		// ordi choisi les couleurs
+
+		code_couleur(jouer2,Nmb_couleurs);
+
+		// Le jeux commence ici
+
+		printf("--C'est a vous de jouer--\n");
+
+		int resultat = masterMind(jouer1,jouer2,Nmb_couleurs);
+
+		if(resultat==1)
+		{
+			printf("Bravo, vous avez gagner\n");
+		}else if(resultat==0)
+		{
+			printf("Vous n'avez plus d'essais\n");
+		}
+	}
+
+	return 0;
+}
+
+
+void code_couleur(char jouer2[][20], int Nmb_couleurs)
+{
+	
+	srand(time(NULL));
+	// L'intervalle des nombres generent est 0 à 11 les bornes inclues
+	
+	for(int i=0; i<Nmb_couleurs; i++)
+	{
+		int nombre = rand() % 11;
+
+		switch(nombre)
+		{
+			case 0 :
+				strcpy(jouer2[i],"rouge");
+				break;
+			case 1 :
+				strcpy(jouer2[i],"bleu");
+				break;
+			case 2 :
+				strcpy(jouer2[i],"vert");
+				break;
+			case 3 :
+				strcpy(jouer2[i],"jaune");
+				break;
+			case 4 :
+				strcpy(jouer2[i],"violet");
+				break;
+			case 5 :
+				strcpy(jouer2[i],"orange");
+				break;
+			case 6 :
+				strcpy(jouer2[i],"rose");
+				break;
+			case 7 :
+				strcpy(jouer2[i],"marron");
+				break;
+			case 8 :
+				strcpy(jouer2[i],"gris");
+				break;
+			case 9 :
+				strcpy(jouer2[i],"noir");
+				break;
+			case 10 :
+				strcpy(jouer2[i],"blanc");
+				break;
+			default:
+				printf("Message: probleme aux niveaux de la fonction code_couleur\n");
+				break;
+		}
+
+		// la suite des couleurs choisi par l'ordi
+		printf("%s,", jouer2[i]);
+	}
+
+
+}
+
+int masterMind(char jouer1[][20], char jouer2[][20], int Nmb_couleurs)
+{
+	
+
+	int essais=3;
+	int iteration=0;
+	int bonne_position=0;
+	int bonne_couleur=0;
+	while(iteration<=essais)
+	{
+		// le joueur entre une combinaison
+		
+		printf("Il vous reste %d essais\n\n", essais-iteration);
+		for(int i=0; i<Nmb_couleurs; i++)
+		{
+			printf("Entrez la couleurs %d de votre choix: ", i);
+			do
+			{	
+				scanf("%19s", jouer1[i]);
+
+				// efface '\n'
+				while(getchar() != '\n');
+
+			}while(strcmp(jouer1[i],"rouge")!=0 && strcmp(jouer1[i],"bleu")!=0 && strcmp(jouer1[i],"vert")!=0 && strcmp(jouer1[i],"jaune")!=0 
+			&&strcmp(jouer1[i],"violet")!=0 && strcmp(jouer1[i],"orange")!=0 && strcmp(jouer1[i],"rose")!=0 &&
+			strcmp(jouer1[i],"marron")!=0 && strcmp(jouer1[i],"gris")!=0 && strcmp(jouer1[i],"noir")!=0 && strcmp(jouer1[i],"blanc")!=0);
+		} 
+
+		//
+
+		int matchedIndices[20] = {0}; // Array to keep track of matched indices
+
+        for (int i = 0; i < Nmb_couleurs; i++)
+        {
+            for (int j = 0; j < Nmb_couleurs; j++)
+            {
+                if (strcmp(jouer1[i], jouer2[j]) == 0 && !matchedIndices[j])
+                {
+                    if (i == j)
+                    {
+                        bonne_position++;
+                        printf("###%s correctement placé###\n", jouer1[i]);
+                    }
+                    else
+                    {
+                        bonne_couleur++;
+                        printf("###%s n'est correctement placé\n###", jouer1[i]);
+                    }
+
+                    matchedIndices[j] = 1; // Mark this index as matched
+                    break;               // Break to avoid multiple matches for the same color
+                }
+            }
+        }	
+		// 
+
+		if(bonne_position==Nmb_couleurs)
+		{
+			return 1;
+		}
+		iteration++;
+	}
+	return 0;
+}	
+
+
+
